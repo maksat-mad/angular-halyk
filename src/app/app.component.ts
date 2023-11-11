9fcab63ed1bee945d453df2e5f83b46599bb8d86
Maksat <maksat.madeniyetov@gmail.com>
2023-11-11 17:00:23 +0600

    feat: bottom menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, Renderer2 } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
+import * as Hammer from 'hammerjs';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +8,19 @@ import * as mapboxgl from 'mapbox-gl';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  @ViewChild('container', { static: true }) container!: ElementRef;
   @ViewChild('mapbox', { static: true }) mapDivElement!: ElementRef;
-  map: mapboxgl.Map | undefined;
-  marker: mapboxgl.Marker | undefined;
+  @ViewChild('menu', { static: true }) menu!: ElementRef;
+  @ViewChild('menuHeader', { static: true }) menuHeader!: ElementRef;
+
+  map!: mapboxgl.Map;
+  marker!: mapboxgl.Marker;
+
+  constructor(private renderer: Renderer2) {
+    setTimeout(() => {
+      this.initHammer();
+    }, 1000)
+  }
 
   ngOnInit() {
     this.map = new mapboxgl.Map({
@@ -17,7 +28,7 @@ export class AppComponent implements OnInit {
       container: this.mapDivElement.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v12',
       center: { lat: 43.226956, lng: 76.944064 },
-      zoom: 12
+      zoom: 14
     });
 
     this.map.addControl(new mapboxgl.NavigationControl());
@@ -27,7 +38,24 @@ export class AppComponent implements OnInit {
       .addTo(this.map);
 
     this.map.on('move', () => {
-      this.marker?.setLngLat(this.map ? this.map.getCenter() : { lat: 43.226956, lng: 76.944064 });
+      this.marker.setLngLat(this.map ? this.map.getCenter() : { lat: 43.226956, lng: 76.944064 });
+    });
+
+    this.map.on('click', () => {
+      this.renderer.setStyle(this.menu.nativeElement, 'height', `${150}px`);
+    });
+  }
+
+  initHammer() {
+    const hammer = new Hammer(this.menuHeader.nativeElement);
+
+    hammer.get('pan').set({ direction: Hammer.DIRECTION_VERTICAL });
+
+    hammer.on('panmove', (event) => {
+      const deltaY = event.center.y;
+      let newHeight = Math.max(150, this.container.nativeElement.clientHeight - deltaY);
+      newHeight = Math.min(newHeight, this.container.nativeElement.clientHeight - 100);
+      this.renderer.setStyle(this.menu.nativeElement, 'height', `${newHeight}px`);
     });
   }
 }
